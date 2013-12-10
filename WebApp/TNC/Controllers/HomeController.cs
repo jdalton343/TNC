485ab8513eb4d262c867d953ab6692ffe1023d5a
Jacob Dalton <jdalton343@gmail.com>
2013-12-10 14:31:17 -0800

    Fixes to Sustainability Model

--- WebApp/TNC/Controllers/HomeController.cs
+++ WebApp/TNC/Controllers/HomeController.cs
@@ -10,32 +10,39 @@ namespace TNC.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.HeadTitle = "Home | True North Composites";
             return View("~/Views/Home/Home.cshtml");
         }
 
         public ActionResult History()
         {
+            ViewBag.HeadTitle = "History | True North Composites";
             return View("~/Views/Home/History.cshtml");
         }
 
         public ActionResult Sustainability_Model()
         {
+            ViewBag.HeadTitle = "Sustainability Model| True North Composites";
             return View();
         }
         public ActionResult Technology()
         {
+            ViewBag.HeadTitle = "Technology| True North Composites";
             return View("~/Views/Home/Technology.cshtml");
         }
         public ActionResult Gallery()
         {
+            ViewBag.HeadTitle = "Gallery | True North Composites";
             return View("~/Views/Home/Gallery.cshtml");
         }
         public ActionResult Contact()
         {
+            ViewBag.HeadTitle = "Contact| True North Composites";
             return View("~/Views/Home/Contact.cshtml");
         }
         public ActionResult Blog()
         {
+            ViewBag.HeadTitle = "Blog| True North Composites";
             return View("~/Views/Home/Blog.cshtml");
         }
     }
