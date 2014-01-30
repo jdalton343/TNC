a73e05da39738cf30bf098146705f94772068f84
Jacob Dalton <jdalton343@gmail.com>
2014-01-30 14:39:48 -0800

    Structure draft for the History View.

--- WebApp/TNC/Controllers/HomeController.cs
+++ WebApp/TNC/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace TNC.Controllers
         public ActionResult History()
         {
             ViewBag.HeadTitle = "History | True North Composites";
-            return View("~/Views/Home/History.cshtml");
+            return View("~/Views/Home/HistoryTemp.cshtml");
         }
 
         public ActionResult Sustainability_Model()
