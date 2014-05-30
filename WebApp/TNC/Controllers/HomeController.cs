6957dc1f03d62f5c2cb9871ed6dcba57d752c245
Aldo Salzberg <aldosalzberg@earthlink.net>
2014-05-29 17:47:53 -0700

    Demo for news page showing static and dynamic versions of the same page.

--- WebApp/TNC/Controllers/HomeController.cs
+++ WebApp/TNC/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using TNC.Models;
+
 namespace TNC.Controllers
 {
     public class HomeController : Controller
@@ -99,6 +101,37 @@ namespace TNC.Controllers
             return View("~/Views/Home/News.cshtml");
         }
 
+        public ActionResult NewsDemo()
+        {
+            ViewBag.HeadTitle = "News | True North Composites";
+            return View("~/Views/Home/NewsDemo.cshtml");
+        }
+
+        public ActionResult NewsDemoDynamic()
+        {
+            ViewBag.HeadTitle = "News | True North Composites";
+            MapViewModel map = new MapViewModel();
+
+            List<Tuple<string, string, string>> mapItems = new List<Tuple<string, string, string>>()
+            {
+                Tuple.Create<string, string, string>("top:25%; left:30%", "Location 1", "location 1 description"),
+                Tuple.Create<string, string, string>("top:45%; left:50%", "Location 2", "location 2 description"),
+                Tuple.Create<string, string, string>("top:75%; left:30%", "Location 3", "location 3 description"),
+                Tuple.Create<string, string, string>("top:35%; left:20%", "Location 4", "location 4 description"),
+            };
+            map.MapItems = mapItems;
+
+            List<Tuple<string, string>> newsItems = new List<Tuple<string, string>>()
+            {
+                Tuple.Create<string, string>("News item 1", "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis eleifend congue. Morbi interdum eu enim eget sagittis. Fusce libero felis, sagittis vel volutpat pretium, sollicitudin in leo. Suspendisse faucibus suscipit ornare. Duis volutpat at mi at vestibulum. Proin sed quam eu erat consectetur placerat. Proin id pulvinar ligula. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed eu diam sit amet libero mollis vehicula eu at magna. Praesent nec lacus laoreet, blandit magna et, iaculis quam. Integer vulputate iaculis hendrerit. Vivamus feugiat, urna eget gravida bibendum, quam lectus porttitor diam, at bibendum massa nibh vel massa. Etiam dapibus magna dolor. Proin consectetur nisi et eros commodo, quis pulvinar turpis venenatis. </p>"),
+                Tuple.Create<string, string>("News item 2", "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis eleifend congue. Morbi interdum eu enim eget sagittis. Fusce libero felis, sagittis vel volutpat pretium, sollicitudin in leo. Suspendisse faucibus suscipit ornare. Duis volutpat at mi at vestibulum. Proin sed quam eu erat consectetur placerat. Proin id pulvinar ligula. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed eu diam sit amet libero mollis vehicula eu at magna. Praesent nec lacus laoreet, blandit magna et, iaculis quam. Integer vulputate iaculis hendrerit. Vivamus feugiat, urna eget gravida bibendum, quam lectus porttitor diam, at bibendum massa nibh vel massa. Etiam dapibus magna dolor. Proin consectetur nisi et eros commodo, quis pulvinar turpis venenatis. </p>"),
+                Tuple.Create<string, string>("News item 3", "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis eleifend congue. Morbi interdum eu enim eget sagittis. Fusce libero felis, sagittis vel volutpat pretium, sollicitudin in leo. Suspendisse faucibus suscipit ornare. Duis volutpat at mi at vestibulum. Proin sed quam eu erat consectetur placerat. Proin id pulvinar ligula. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed eu diam sit amet libero mollis vehicula eu at magna. Praesent nec lacus laoreet, blandit magna et, iaculis quam. Integer vulputate iaculis hendrerit. Vivamus feugiat, urna eget gravida bibendum, quam lectus porttitor diam, at bibendum massa nibh vel massa. Etiam dapibus magna dolor. Proin consectetur nisi et eros commodo, quis pulvinar turpis venenatis. </p>"),
+                Tuple.Create<string, string>("News item 4", "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis eleifend congue. Morbi interdum eu enim eget sagittis. Fusce libero felis, sagittis vel volutpat pretium, sollicitudin in leo. Suspendisse faucibus suscipit ornare. Duis volutpat at mi at vestibulum. Proin sed quam eu erat consectetur placerat. Proin id pulvinar ligula. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed eu diam sit amet libero mollis vehicula eu at magna. Praesent nec lacus laoreet, blandit magna et, iaculis quam. Integer vulputate iaculis hendrerit. Vivamus feugiat, urna eget gravida bibendum, quam lectus porttitor diam, at bibendum massa nibh vel massa. Etiam dapibus magna dolor. Proin consectetur nisi et eros commodo, quis pulvinar turpis venenatis. </p>"),
+            };
+            map.NewsItems = newsItems;
+
+            return View("~/Views/Home/NewsDemoDynamic.cshtml", map);
+        }
 
         public ActionResult Attributions()
         {
---
+++ WebApp/TNC/Models/MapViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TNC.Models
+{
+    public class MapViewModel
+    {
+        public List<Tuple<string, string, string>> MapItems { get; set; }
+
+        public List<Tuple<string, string>> NewsItems { get; set; }
+    }
+}
