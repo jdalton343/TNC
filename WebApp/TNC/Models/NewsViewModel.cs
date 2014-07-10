290a275b6d72045312a7aac8a5d331e30f084cc9
Aldo Salzberg <aldosalzberg@earthlink.net>
2014-07-10 16:31:26 -0700

    Demo on how to call database from EF 5.0. Note that you must configure web.config connection strings for your own system. Also, you need to run the latest database file or no data will be found.

---
+++ WebApp/TNC/Models/NewsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TNC.Models
+{
+    public class NewsViewModel
+    {
+        public List<NewsItem> NewsItems { get; set; }
+
+    }
+}
